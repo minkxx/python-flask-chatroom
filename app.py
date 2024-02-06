89720260d01ed39616b6f679a0dfbfb09294bfd1
minkxx <aryuokk@gmail.com>
2024-02-06 14:29:57 +0530

    add :

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
@@ -6,11 +6,11 @@ app.secret_key = "abcdefg"
 
 @app.route("/")
 def home_page():
-    return render_template("room.html", room_code="Under development")
-
-@app.route("/login")
-def login_page():
     return render_template("login.html")
+
+@app.route("/room", methods=["POST","GET"])
+def login_page():
+    return render_template("room.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
